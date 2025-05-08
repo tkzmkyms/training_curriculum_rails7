47b1b7b13a13675cd41213135db79dd0188a5669
tkzmkyms <tkzmkyms1002@gmail.com>
2025-05-08 11:44:01 +0900

    Issue2
    l21 2-1 getWeek / get_week
    
    l2 2-2 def index
                getWeek/get_week

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,8 +1,8 @@
 class CalendarsController < ApplicationController
-
+  #issue2-2 getWeek → get_week
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_week
     @plan = Plan.new
   end
 
@@ -17,8 +17,9 @@ class CalendarsController < ApplicationController
   def plan_params
     params.require(:calendars).permit(:date, :plan)
   end
-
-  def getWeek
+  
+  #issue2-1 def getWeekを修正→動詞から始まるスネークケースに→get_week
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -34,7 +35,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      #issue1の箇所_days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      #issue1 days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans }
       @week_days.push(days)
     end
