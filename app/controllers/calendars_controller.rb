aca9cdbdf2eeee00d209055b605b2bfe25813442
tkzmkyms <tkzmkyms1002@gmail.com>
2025-05-09 12:31:32 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,13 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      #issue6-2_3  2曜日を取得を追加 3条件式追加
+      wday_num = (@todays_date + x).wday   # wdayメソッドを用いて取得した数値
+      if wday_num >= 7   #「wday_numが7以上の場合」という条件式
+        wday_num = wday_num -7
+      end
+      #issue6-1 曜日をindex.html.erbに渡す
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :wday  => wdays[(@todays_date + x).wday], :plans => today_plans}
       @week_days.push(days)
     end
 
